3d2a5e2aa1cf4c78d14165c47533b11aa3c77735
Luba <luciano.eq@outlook.com>
2022-03-31 04:39:58 -0300

    :sparkles: backend and frontend applications.

---
+++ backend/main.py
@@ -0,0 +1,32 @@
+import uuid
+
+import cv2
+import uvicorn
+from fastapi import File, FastAPI, UploadFile
+import numpy as np
+from PIL import Image
+
+import config
+import inference
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome from the API"}
+
+
+@app.post("/{style}")
+def get_image(style: str, file: UploadFile = File(...)):
+    image = np.array(Image.open(file.file))
+    model = config.STYLES[style]
+    output, resized = inference.inference(model, image)
+    name = f"/storage/{str(uuid.uuid4())}.jpg"
+    cv2.imwrite(name, output)
+    return {"name": name}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
